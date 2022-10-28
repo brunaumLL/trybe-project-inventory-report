ecc72d49286f9db1fe394e950b0045965f07b917
brunaumLL <bruno99louzada@gmail.com>
2022-10-28 16:32:44 -0300

    req 01 feito

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,19 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Borracha",
+        "Papelaria Solar",
+        "2021-07-04",
+        "2029-02-09",
+        "FR48",
+        "Ao abrigo de luz solar")
+    assert product.id == int(1)
+    assert product.nome_do_produto == str("Borracha")
+    assert product.nome_da_empresa == str("Papelaria Solar")
+    assert product.data_de_fabricacao == str("2021-07-04")
+    assert product.data_de_validade == str("2029-02-09")
+    assert product.numero_de_serie == str("FR48")
+    assert product.instrucoes_de_armazenamento == str("Ao abrigo de luz solar")
