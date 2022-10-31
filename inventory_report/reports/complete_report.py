d4bf4784bcb9ed28c0991853007fe3b61e12aa54
brunaumLL <bruno99louzada@gmail.com>
2022-10-31 17:59:08 -0300

    req 04 feito

---
+++ inventory_report/reports/complete_report.py
@@ -0,0 +1,22 @@
+from collections import Counter
+from .simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(a, list):
+        simple_report = super().generate(list)
+        company_stock = dict(
+            Counter([product["nome_da_empresa"] for product in list]))
+
+        company_stock_quantity = ""
+
+        for company in company_stock:
+            company_stock_quantity += (
+                f"- {company}: {company_stock[company]}\n")
+
+        return(
+            f"{simple_report}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{company_stock_quantity}"
+        )
