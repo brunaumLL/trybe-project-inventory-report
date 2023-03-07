ff3c0215294398a0ebbf366ef07f23c74fad9b48
brunaumLL <bruno99louzada@gmail.com>
2023-03-07 16:09:51 -0300

    requisito 02 feito

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        'Lápis Bonito',
+        'Empresa Bonita',
+        '2023-01-01',
+        '2024-01-01',
+        'A0B1',
+        'Guarde com carinho'
+    )
+    result = product.__repr__()
+    assert result == (
+            f"O produto {product.nome_do_produto}"
+            f" fabricado em {product.data_de_fabricacao}"
+            f" por {product.nome_da_empresa} com validade"
+            f" até {product.data_de_validade}"
+            f" precisa ser armazenado {product.instrucoes_de_armazenamento}."
+        )
