806e1c4c4cebde2c47ed82c264fd9c3995da9580
brunaumLL <bruno99louzada@gmail.com>
2023-03-07 20:11:28 -0300

    requisito 06 feito

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,14 +1,18 @@
 import csv
+import json
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
 
 class Inventory:
     @classmethod
-    def import_data(cls, file_csv, type_report):
-        if file_csv.endswith('csv'):
-            with open(file_csv, encoding="utf-8") as file:
+    def import_data(cls, any_file, type_report):
+        if any_file.endswith('csv'):
+            with open(any_file, encoding="utf-8") as file:
                 [*products_list] = csv.DictReader(file)
+        if any_file.endswith('.json'):
+            with open(any_file, encoding="utf-8") as file:
+                products_list = json.load(file)
 
         if type_report == "simples":
             return SimpleReport.generate(products_list)
