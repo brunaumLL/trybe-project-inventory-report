c62d0d2dcc8ba68ee17052377518888e2c51e215
brunaumLL <bruno99louzada@gmail.com>
2023-03-07 20:23:09 -0300

    requisito 07 feito

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import xmltodict
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -10,9 +11,14 @@ class Inventory:
         if any_file.endswith('csv'):
             with open(any_file, encoding="utf-8") as file:
                 [*products_list] = csv.DictReader(file)
-        if any_file.endswith('.json'):
+        elif any_file.endswith('.json'):
             with open(any_file, encoding="utf-8") as file:
                 products_list = json.load(file)
+        else:
+            with open(any_file, encoding="utf-8") as file:
+                my_xml = file.read()
+                my_list = xmltodict.parse(my_xml)
+                products_list = my_list["dataset"]["record"]
 
         if type_report == "simples":
             return SimpleReport.generate(products_list)
